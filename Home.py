66b200141bdf1b18cced414a56e9a35812b44826
FedeClaramunt10 <fedeclaramunt@gmail.com>
2026-07-02 20:02:51 -0300

    primer commit

---
+++ Home.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.set_page_config(page_title="Proyecto Integrador - Minería de Datos I", page_icon="📊", layout="wide")
+st.title("🚀 Título de tu Proyecto Analítico")
+st.write("**Integrantes:** Nombre Integrante 1, Nombre Integrante 2")
